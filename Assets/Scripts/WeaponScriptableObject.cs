781046c87d9c3420abbaeeec5cae3c4eb1a97b92
BigBoss435 <deividas.kalvelis@gmail.com>
2023-02-19 12:19:18 +0200

    Added enemy AI and scriptable objects

--- Assets/Scripts/WeaponScriptableObject.cs
+++ Assets/Scripts/WeaponScriptableObject.cs
@@ -1,3 +1,14 @@
-﻿public class WeaponScriptableObject
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponSrcriptableObject", menuName = "ScriptableObjects/Weapon")]
+public class WeaponScriptableObject : ScriptableObject
 {
+    public GameObject prefab;
+    //Base stats
+    public float damage;
+    public float speed;
+    public float cooldownDuration;
+    public int pierce;
 }
